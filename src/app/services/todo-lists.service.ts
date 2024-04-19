cc5ac4f6dac4688f02a98370f21f57be1060910f
James <Jamestngo02@gmail.com>
2024-04-18 22:38:49 -0400

    using http client and getting todolist from api endpoints

--- src/app/services/todo-lists.service.ts
+++ src/app/services/todo-lists.service.ts
@@ -10,11 +10,32 @@ export class TodoListsService {
 
 
   //lets have an empty array and declare our http client
+  //use http client to communicate the backend
+  //aval to service to use 
   constructor(private httpClient:HttpClient) {
     
    }
 
+
+   //replace with call to the end point now got http client 
    getTodoList():string[]{
+    //sits in callback form server 
+
+    //async
+    this.httpClient.get('https://unfwfspring2024.azurewebsites.net/todo/').subscribe((data:any)=>{
+
+    for(let row of data){
+      this.todoLists.push(row.title);
+      console.log(row.title);
+    }
+      
+   });
+
+
+
+
+    console.log("this is the todolist");
+    console.log(this.todoLists);
     return this.todoLists;
    }
 
